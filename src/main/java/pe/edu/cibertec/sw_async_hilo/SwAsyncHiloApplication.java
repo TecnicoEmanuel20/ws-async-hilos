59b544c19b3bbaf9302b7280417e5d9089ecaf76
TecnicoEmanuel20 <perezanchayaedgaremanuel@gmal.com>
2024-09-29 09:10:12 -0500

    initial project

---
+++ src/main/java/pe/edu/cibertec/sw_async_hilo/SwAsyncHiloApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_async_hilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwAsyncHiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwAsyncHiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/sw_async_hilo/SwAsyncHiloApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_async_hilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwAsyncHiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
